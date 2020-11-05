b59c390a0b8a025863183cdab4ad0288da79b20f
rejth <kirsanovia@yandex.ru>
2020-11-05 22:24:59 +0300

    created tasks with gulp-notify/del plugins & build project

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,56 @@
-const gulp = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const imageMin= require('gulp-imagemin');
+const imageMin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const del = require('del');
 
-// Create static server + watching sass/html/js/img files
-gulp.task('server', function() {
+// Create static server
+function serverTask() {
     browserSync.init({
         server: {
             baseDir: "./src"
         },
         notify: true
     });
-    gulp.watch("src/sass/**/*.sass", gulp.series('sass'));
-    gulp.watch("src/**/*.html").on('change', browserSync.reload);
-    gulp.watch("src/js/**/*.js").on('change', gulp.series('scripts'));
-    gulp.watch("src/img/**/*").on('change',  browserSync.reload);
-    gulp.watch("src/sass/**/img/*").on('change',  browserSync.reload);
-});
+};
 
-// Compile SASS into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-    return gulp.src("src/sass/**/*.sass")
-        .pipe(sass())
+// Watching html/css/js/img files
+function watchTask() {
+    watch("src/**/*.html").on('change', browserSync.reload);
+    watch("src/css/**/*.css").on('change', browserSync.reload);
+    watch("src/js/**/*.js").on('change', browserSync.reload);
+    watch("src/img/**/*").on('change',  browserSync.reload);
+};
+
+// Compile SASS into CSS, autoprefix, minify & load to dist directory
+function sassTask() {
+    return src("src/sass/**/*.sass")
+        .pipe(sass().on("error", notify.onError()))
         .pipe(autoprefixer(['last 15 versions']))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
+        .pipe(dest("dist/css"));
+};
 
-// Concatenation of JS files into one file & minify it
-gulp.task('scripts', function () {
-    return gulp.src([
+// Concatenation of all JS files, minify & load to the dist directory
+function scriptsTask() {
+    return src([
         "src/js/jquery-3.5.1.js",
         "src/js/script.js"
     ])
         .pipe(concat("script-min.js"))
         .pipe(uglify())
-        .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
-});
+        .pipe(dest("dist/js"));
+};
 
-// Minify images
-gulp.task('images', function () {
-    return gulp.src([
+// Minify, optimize all images & load to the dist directory
+function imagesTask() {
+    return src([
         "src/img/**/*",
-        "src/sass/**/img/*"
     ])
         .pipe(imageMin([
             imageMin.mozjpeg({quality: 75, progressive: true}),
@@ -60,7 +62,26 @@ gulp.task('images', function () {
                 ]
             })
         ]))
-        .pipe(gulp.dest("dist"));
-});
+        .pipe(dest("dist/img"));
+}
 
-gulp.task('default', gulp.parallel('server'));
+// Delete dist directory
+function deleteTask() {
+    return del(["dist"]);
+};
+
+// Build project & load to the dist directory
+function buildTask() {
+    return src("src/**/*.html")
+        .pipe(dest("dist"))
+}
+
+exports.server = serverTask;
+exports.watch = watchTask;
+exports.del = deleteTask;
+exports.sass = sassTask;
+exports.scripts = scriptsTask;
+exports.images = imagesTask;
+
+exports.build = series(deleteTask, sassTask, scriptsTask, imagesTask, buildTask);
+exports.default = parallel(serverTask, watchTask);
